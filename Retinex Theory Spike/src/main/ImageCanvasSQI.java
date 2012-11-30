32a12afc03232f9b3095cd5653278c324ea26b08
James-sama <James-sama@James-sama-PC>
2012-11-30 20:02:49 +0000

    Moving project over to desktop.

--- Retinex Theory Spike/src/main/ImageCanvasSQI.java
+++ Retinex Theory Spike/src/main/ImageCanvasSQI.java
@@ -20,6 +20,15 @@ public class ImageCanvasSQI extends JPanel implements ActionListener {
 		private JFileChooser chooser;//for loading image using a file chooser.
 		private final static String[] extensionsAllowedJFileChooser = new String[]{"gif","jpg","jpeg","png"};
 		private final static String descriptionJFileChooser = "Image files *.gif, *.png, *.jpeg, *.jpg";
+
+    	
+    	private int totalRows = 0;
+    	private int totalCols = 0;
+    	private int[] bufferedImageData;
+    	
+    	private short[][] bufferedImageRed = null;
+    	private short[][] bufferedImageGreen = null;
+    	private short[][] bufferedImageBlue = null;
 		
 		private BufferedImage loadedImage, processedImage; //image object of input image
 		
@@ -74,6 +83,7 @@ public class ImageCanvasSQI extends JPanel implements ActionListener {
 				imageName = dir.getPath();//Get's the directory of the file as a string.
 				
 				loadedImage = MyImageUtilities.getBufferedImage(imageName, retinexSQI);
+				unpackImage(loadedImage);
 			}
 			else{
 				System.out.println("Nothing Selected!");
@@ -81,19 +91,15 @@ public class ImageCanvasSQI extends JPanel implements ActionListener {
 		}
 	
 		public void SaveImage(){
-			processedImage = loadedImage;
-           File savedFile=new File("test.jpg");
-           try {
-           ImageIO.write(processedImage,"jpeg",savedFile);
-           System.out.println("Image Saved!");
-           } catch (Exception e) {}
+			processedImage = packImage(bufferedImageRed, bufferedImageGreen, bufferedImageBlue);
+            File savedFile=new File("test.jpg");
+            try {
+            ImageIO.write(processedImage,"jpeg",savedFile);
+            System.out.println("Image Saved!");
+            } catch (Exception e) {}
 		}
 		
-		public void Unpack(){//Unpacks the image.
-			
-			
-			
-		}
+		
 		
 		public void paint(Graphics g) {//Draws the image on the screen.
 			boolean x = false;
@@ -102,8 +108,87 @@ public class ImageCanvasSQI extends JPanel implements ActionListener {
 	        if (loadedImage != null) {
 	        	while(!x) x = g.drawImage(loadedImage, 0, 0, null);
 	        }
-
+	    
 		}
+		
+		
+	    /** It takes a 24-bit pixel in the form 0xRRGGBB
+	    * and pulls out the three components, storing
+	    * them position (row, col) or the red, green,
+	    * and blue arrays.
+	    * Taken from RetinexSQI.java for CS24110.
+		*/
+	    private static void unpackPixel(int pixel, short [][] red, short [][] green, short [][] blue, int row, int col) {
+	        red[row][col]   = (short)((pixel >> 16) & 0xFF);
+	        green[row][col] = (short)((pixel >>  8) & 0xFF);
+	        blue[row][col]  = (short)((pixel >>  0) & 0xFF);
+	    }
+	    
+	    
+	    /**
+	     * 
+	     * 
+	     * 
+	     */
+	    private static int packPixel(int red, int green, int blue) {
+	        return (red << 16) | (green << 8) | blue;
+	    }
+	    
+	    public void unpackImage(BufferedImage bufferedImage) {
+	        // check if we need to resize the component arrays, i.e.,
+	        // has the size of the image changed?
+
+			
+			if (bufferedImage.getHeight() != totalRows || bufferedImage.getWidth() != totalCols) {
+	            totalRows = bufferedImage.getHeight();
+	            totalCols = bufferedImage.getWidth();
+
+	            bufferedImageRed   = new short[totalRows][totalCols];
+	            bufferedImageGreen = new short[totalRows][totalCols];
+	            bufferedImageBlue  = new short[totalRows][totalCols];
+	        }
+
+	        // get pixels as ints of the form 0xRRGGBB
+	        bufferedImageData = bufferedImage.getRGB(0, 0, bufferedImage.getWidth(),
+	                                           bufferedImage.getHeight(), null, 0,
+	                                           bufferedImage.getWidth());
+
+	        // extract red, green, and blue components from each pixel
+	        int index;
+	        for (int currentRow = 0; currentRow < totalRows; currentRow++) {
+	            for (int currentCol = 0; currentCol < totalCols; currentCol++) {
+	                index = (currentRow * totalCols) + currentCol;
+	                unpackPixel(bufferedImageData[index], bufferedImageRed, bufferedImageGreen, bufferedImageBlue, currentRow, currentCol);
+	            }
+	        }
+	    }
+	    
+	    public BufferedImage packImage(short[][] processedImageRed,
+                								short[][] proccesedImageGreen,
+                								short[][] processedImageBlue ) {
+	    	
+			int[] newBufferedImageData = new int[totalRows * totalCols];
+	    	int index;
+	    	for (int currentRow = 0; currentRow < totalRows; currentRow++) {
+	    		for (int currentCol = 0; currentCol < totalCols; currentCol++) {
+	    			index = (currentRow * totalCols) + currentCol;
+	    			newBufferedImageData[index] = packPixel(processedImageRed[currentRow][currentCol],
+	    										proccesedImageGreen[currentRow][currentCol],
+	    										processedImageBlue[currentRow][currentCol]
+	    										);
+	    		}
+	    	}
+
+	    	BufferedImage newImage = new BufferedImage(totalCols, totalRows, BufferedImage.TYPE_INT_RGB);
+	    	for (int row = 0; row < totalRows; row++) {
+	    		for (int col = 0; col < totalCols; col++) {
+	    			index = (row * totalCols) + col;
+	    			newImage.setRGB(col, row, newBufferedImageData[index]);
+	    		}
+	    	}
+
+	    	return newImage;
+	    }
 		
 		
 	
